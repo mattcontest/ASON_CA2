d489fac3d5830c0262a9d658f9b9ae73f5dcc34a
mattco <marslite@pm.me>
2026-03-17 18:16:23 +0000

    Updating Menu with switch case base

--- menu.sh
+++ menu.sh
@@ -1,15 +1,46 @@
 #! /bin/sh
 INPUT_STRING=""
 
+clearScreen(){
+    clear
+}
+
 while [ "$INPUT_STRING" != "quit" ]
 do
-    echo "Please type something in (quit to exit) " 
-    read INPUT_STRING
-    echo "You typed: $INPUT_STRING"
+    clearScreen
+    echo "-----------------------------"
+    echo "     --Website Panel--       "
+    echo "-----------------------------"
+    echo "1. List Users"
+    echo "2. Add User to WebGroup"
+    echo "3. Backup Intranet Directory"
+    echo "4. Backup Live Directory"
+    echo "5. Transfer Updates to Website"
+    echo "6. Audit Reports"
+    echo "7. System Health Reports"
+    echo "q. Quit"
+    echo "Enter your choice :  "
+    
+    read userChoice
+    # clear
+    case $userChoice in
+        1) echo "Running Option 1";;
+        2) echo "Running Option 2";;
+        3) echo "Running Option 3";;
+        4) echo "Running Option 4";;
+        5) echo "Running Option 5";;
+        6) echo "Running Option 6";;
+        7) echo "Running Option 7";;
+        q) break;;
+        *) echo "Invalid Option";;
+    esac
+    
+
+    echo "Press any key to continue: "
+    read key
+    # clear
+
 done
-
-
-
 
 
 
